d982eb63464414c431192641a72d2771c374a26e
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-03-02 13:03:42 +0100

    Update script.js

--- Pokemon/script.js
+++ Pokemon/script.js
@@ -1,17 +1,17 @@
-function fetchPokemon(pokemon) {
-  return fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
+function fetchPokemon(pokemon) { //fetchPokemon est une fonction qui prend en paramètre un pokemon
+  return fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon) // fetch les données de l'API du pokemon
     .then(response => response.json())
 }
 
-async function displayPokemon (pokemon){
-    const data = await fetchPokemon(pokemon)
-    const container_card = document.querySelector(".pokemon_container")
+async function displayPokemon (pokemon){ 
+    const data = await fetchPokemon(pokemon) // on stocke les données dans une variable
+    const container_card = document.querySelector(".pokemon_container") 
 
     container_card.innerHTML = 
     `
         <h3>${data.name}</h3>
-        <img src="${data.sprites.front_default}" alt="${data.name}">
+        <img src="${data.sprites.front_default}" alt="${data.name}"> 
     `    
 }
 
-displayPokemon('ditto')
+displayPokemon('ditto') // on appelle la fonction displayPokemon avec le nom du pokemon en paramètre
